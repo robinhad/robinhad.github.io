70bd0f32bf3c49c702ae252bbf76f9e638aa8b8e
Yurii Paniv <mr.robinhad@gmail.com>
2025-02-18 13:25:22 +0200

    Bump DPI for thumbnails

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,3 @@
 for file in images/*.{jpg,png}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 160x160 "tn/$file"
+    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 320x320 "tn/$file"
 done
