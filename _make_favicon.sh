7818cef85769ca864f82e051abc43f9774602093
Yurii Paniv <mr.robinhad@gmail.com>
2025-02-18 14:22:13 +0200

    Update favicon

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,7 @@
-convert -background transparent "images/circle_bw_crop.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+convert "images/circle_bw_crop.jpg" \
+        -alpha set -background none \
+        -vignette 0x0+0+0 \
+        -define icon:auto-resize=16,24,32,48,64,72,96,128,256 \
+        "favicon.ico"
+
+#convert -background transparent "images/circle_bw_crop.jpg" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"        
